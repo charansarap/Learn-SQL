5057e16f81f779a50348eaeff6339e8e395d713e
charansarap <84665458+charansarap@users.noreply.github.com>
2021-07-18 13:49:31 +0530

    Update 5.sql

--- solutions/5.sql
+++ solutions/5.sql
@@ -3,3 +3,9 @@ FROM bands
 LEFT JOIN albums ON bands.id = albums.band_id
 GROUP BY albums.band_id
 HAVING COUNT(albums.id) = 0;
+
+#without using joins
+
+select name from bands
+where id not in(select band_id from albums)
+
